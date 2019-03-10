4868661acf4f91624fbe7148778c9148aa583e55
adarshrana205 <42955521+adarshrana205@users.noreply.github.com>
2019-03-10 23:37:37 +0530

    Update sketch_mar09a.ino

--- sketch_mar09a/sketch_mar09a.ino
+++ sketch_mar09a/sketch_mar09a.ino
@@ -1,19 +1,18 @@
 #include <Servo.h>. 
 const int trigPin = 11;
 const int echoPin = 12;
-// defining time and distance
+
 long duration;
 int distance;
-Servo myServo; // Object servo
+Servo myServo; 
 void setup() {
   Serial.begin(57600);
-  pinMode(trigPin, OUTPUT); // trigPin as an Output
-  pinMode(echoPin, INPUT); // echoPin as an Input
+  pinMode(trigPin, OUTPUT); 
+  pinMode(echoPin, INPUT);
   
-  myServo.attach(13); // Pin Connected To Servo
+  myServo.attach(13); 
 }
 void loop() {
-  // rotating servo i++ depicts increment of one degree
   for(int i=15;i<=165;i++){  
   myServo.write(i);
   delay(5);
@@ -24,7 +23,6 @@ void loop() {
   Serial.print(distance); 
   Serial.print(".");
   }
-  // Repeats the previous lines from 165 to 15 degrees
   for(int i=165;i>15;i--){  
   myServo.write(i);
   delay(5);
@@ -39,7 +37,7 @@ int calculateDistance(){
   
   digitalWrite(trigPin, LOW); 
   delayMicroseconds(2);
-  // Sets the trigPin on HIGH state for 10 micro seconds
+ 
   digitalWrite(trigPin, HIGH); 
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
